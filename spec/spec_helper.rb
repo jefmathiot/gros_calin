f3e4581d1526b3b1abb32de98a61dd02a836ac96
Jef Mathiot <jeff.mathiot@gmail.com>
2015-02-18 17:17:11 +0100

    Add missing spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,19 @@
+require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
+
+SimpleCov.start do
+  add_filter '/spec/'
+end
+
+require 'gros_calin'
+require 'minitest'
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'minitest-implicit-subject'
+require 'mocha/setup'
